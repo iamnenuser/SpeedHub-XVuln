e1e1aa41e137ed615e7402e7e216f7d7560a7898
iamnenuser <132814028+iamnenuser@users.noreply.github.com>
2025-09-09 23:30:37 +0800

    Create GrowAGardenV73.lua

---
+++ GrowAGardenV73.lua
@@ -0,0 +1,5 @@
+if queue_on_teleport then
+    queue_on_teleport([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/iamnenuser/SpeedHub-XVuln/main/GrowAGardenV71.lua"))()
+    ]])
+end
