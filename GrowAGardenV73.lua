1704b936a03cb3147012def2f23d9d95a714e877
iamnenuser <132814028+iamnenuser@users.noreply.github.com>
2025-09-10 00:55:32 +0800

    Update GrowAGardenV73.lua

--- GrowAGardenV73.lua
+++ GrowAGardenV73.lua
@@ -1,5 +1,5 @@
-if queue_on_teleport then
-    queue_on_teleport([[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/iamnenuser/SpeedHub-XVuln/main/GrowAGardenV71.lua"))()
-    ]])
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamnenuser/SpeedHub-XVuln/refs/heads/main/PatchedGrowAGarden.lua"))()
+
+queue_on_teleport([[
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/iamnenuser/SpeedHub-XVuln/refs/heads/main/PatchedGrowAGarden.lua"))()
+]])
